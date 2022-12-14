7fc380963621d35162498da6c5ab518045597da5
ansh007sharma <101504709+ansh007sharma@users.noreply.github.com>
2022-12-14 23:05:41 +0530

    Create  Count Odd Numbers in an Interval Range.cpp

---
+++ Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,44 @@
+# Problem-Solution:-
+  
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+*Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+*Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+
+*Constraints:
+
+*0 <= low <= high <= 10^9
+
+
+# Solution:-
+  
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int count =0;
+        if(low%2!=0){
+        for(int i = low;i <= high;i=i+2)
+        {
+            count++;
+        }
+        }
+        else if(low %2==0)
+        {
+            for(int j = low+1 ; j<=high;j= j+2)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+  
